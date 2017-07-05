94586f0c36db534b4d3570e26a55ce4628fbd983
JonSn0w <therealbrettstevenson@gmail.com>
2017-07-05 06:06:03 -0700

    :rocket: Improve modular app functionalities

--- js/functions.js
+++ js/functions.js
@@ -1,42 +1,42 @@
 var clkPref = function (opt) {
   currentValue = opt.value;
-  if ( currentValue=='preview' ) {
+  if (currentValue=='preview') {
     document.getElementById("htmlPreview").style.display = "none";
     document.getElementById("markdown").style.display = "block";
-  } else if ( currentValue=='html' ) {
+    document.getElementById("previewPanel").style.paddingTop = "25px";
+    document.getElementById("previewPanel").style.paddingRight = "15px";
+  } else if (currentValue=='html') {
     document.getElementById("markdown").style.display = "none";
     document.getElementById("htmlPreview").style.display = "block";
+    document.getElementById("previewPanel").style.paddingTop = "0px";
+    document.getElementById("previewPanel").style.paddingRight = "0px";
   }
 }
 
 var changeTheme = function (opt) {
   currentValueTheme = opt.value;
-  if ( currentValueTheme=='dark' ) {
-    cm.setOption("theme", "monokai");
+  if (currentValueTheme=='light') {
+    cm.setOption("theme", "default");
+    document.getElementById("previewPanel").className = "col-md-6 full-height";
+  } else if (currentValueTheme=='dark') {
+    cm.setOption("theme", "zenburn");
     document.getElementById("previewPanel").className = "col-md-6 full-height preview-dark-mode";
-    document.getElementByClassName("CodeMirror-gutters").borderRight = "1px solid rgba(255,255,255,0.5)"
-  } else if ( currentValueTheme=='light' ) {
-    cm.setOption("theme", "solarized");
-    document.getElementById("previewPanel").className = "col-md-6 full-height preview-light-mode";
-    document.getElementByClassName("CodeMirror-gutters").borderRight = "1px solid rgba(0,0,0,0.5)"
   }
 }
 
 var showToolBar = function () {
-  if(document.getElementById("toolbarArea").style.display == "block") {
+  if(document.getElementById("toolbarArea").style.display == "block"){
     document.getElementById("angleToolBar").className = "";
-    document.getElementById("angleToolBar").className = "fa fa-chevron-right";
+    document.getElementById("angleToolBar").className = "fa fa-angle-right";
     document.getElementById("toolbarArea").style.display = "none";
-    document.getElementById("editArea").style.paddingTop = "24px";
-    document.getElementById("textPanel").style.paddingTop = "10px";
-    document.getElementByClassName("CodeMirror").paddingTop = "3px";
+    document.getElementById("editArea").style.paddingTop = "60px";
+    document.getElementById("textPanel").style.paddingTop = "25px";
   } else {
     document.getElementById("angleToolBar").className = "";
-    document.getElementById("angleToolBar").className = "fa fa-chevron-down";
+    document.getElementById("angleToolBar").className = "fa fa-angle-down";
     document.getElementById("toolbarArea").style.display = "block";
-    document.getElementById("editArea").style.paddingTop = "53px";
-    document.getElementById("textPanel").style.paddingTop = "15px";
-    document.getElementByClassName("CodeMirror").paddingTop = "12px";
+    document.getElementById("editArea").style.paddingTop = "110px";
+    document.getElementById("textPanel").style.paddingTop = "0px";
   }
 }
 
@@ -55,7 +55,7 @@ var isClean = function () {
 
 // Update window title on various events
 var updateWindowTitle = function (path) {
-  var appName = "Hyde MD",
+  var appName = "Hyde",
       isClean = this.isClean(),
       saveSymbol = "*",
       parsedPath,
@@ -65,9 +65,11 @@ var updateWindowTitle = function (path) {
   if (path) {
     parsedPath = parsePath(path);
     dir = parsedPath.dirname || process.cwd();
-    title = parsedPath.basename + " - " + dir + " - " + appName;
+    title = appName + " - " + parsedPath.basename + " - " + dir;
+    document.getElementById("bottom-file").innerHTML = parsedPath.basename;
   } else {
-    title = "New document - " + appName;
+    title = appName + " - New document";
+    document.getElementById("bottom-file").innerHTML = "New document"
   }
   if (!this.isClean()) {
     title = saveSymbol + title;
