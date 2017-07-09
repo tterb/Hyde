f9c049095cb6f77bda0ab0bff466a3657fb2e7cb
JonSn0w <therealbrettstevenson@gmail.com>
2017-07-08 22:57:39 -0700

    :sake: Add live-reload and SCSS compilation to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,19 @@
 
 const fs = require('fs');
 const gulp = require('gulp');
+const scss = require('gulp-scss');
 const packager = require('electron-packager');
-
+const electron = require('electron-connect').server.create();
+const plumber = require('gulp-plumber');
+const jetpack = require('fs-jetpack');
 const config = JSON.parse(fs.readFileSync('package.json'));
 const appVersion = config.version;
-const electronVersion = config.devDependencies['electron-prebuilt'].match(/[\d.]+/)[0];
+// const electronVersion = config.devDependencies['electron'].match(/[\d.]+/)[0];
+
+const projectDir = jetpack;
+const srcDir = jetpack.cwd('./');
+const destDir = jetpack.cwd('./');
+
 const options = {
 	asar: true,
 	dir: '.',
@@ -15,9 +23,35 @@ const options = {
 	out: 'dist',
 	overwrite: true,
 	prune: true,
-	version: electronVersion,
+	// version: electronVersion,
 	'app-version': appVersion
 };
+
+gulp.task('liveReload', () => {
+	electron.start();
+	//Watch js files and restart Electron if they change
+	gulp.watch(['./js/*.js'], electron.restart);
+	//watch css files, but only reload (no restart necessary)
+	gulp.watch(['./css/*.css'], electron.reload);
+	//watch html
+	gulp.watch(['./index.html'], electron.reload);
+});
+
+
+gulp.task('compile-scss', () => {
+    gulp.src(
+            "css/*.scss"
+        ).pipe(scss(
+            {"bundleExec": true}
+        )).pipe(gulp.dest("css/style.css"));
+});
+
+gulp.task('scss', () => {
+  return gulp.src(srcDir.path('css/style.scss'))
+  .pipe(plumber())
+  .pipe(scss())
+  .pipe(gulp.dest(destDir.path('css')));
+});
 
 gulp.task('build:osx', (done) => {
 	options.arch = 'x64';
@@ -42,4 +76,6 @@ gulp.task('build:windows', () => {
 	// @TODO
 });
 
-gulp.task('build', ['build:osx', 'build:linux', 'build:windows']);
+gulp.task('build', ['build:osx', 'build:linux', 'build:windows', 'compile-scss']);
+
+gulp.task('watch', ['scss', 'liveReload']);
