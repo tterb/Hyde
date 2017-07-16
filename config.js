920c06f54fc531623c8e17b7ae234930ea792bb6
JonSn0w <therealbrettstevenson@gmail.com>
2017-07-16 07:37:01 -0700

    :nut_and_bolt: Add additional config settings

--- config.js
+++ config.js
@@ -1,11 +1,25 @@
 'use strict';
 const Config = require('electron-config');
 
-module.exports = new Config({
+module.exports = new Config ({
   defaults: {
-    darkMode: true,
-    isSyncScroll: true,
-    isHtml: false,
-    keepInTray: false
+    firstLaunch: false,
+    showMenu: false,
+    showToolbar: false,
+    livePreview: true,
+    syncScroll: false,
+    keepInTray: false,
+    editorTheme: 'one-dark',
+    editorFont: 'default',
+    editorFontSize: 10,
+    editorLineHeight: 1.5,
+    tabSize: 2,
+    lineNumbers: false,
+    lineWrapping: true,
+    previewMode: 'markdown',
+    previewFont: 'default',
+    previewFontSize: 10,
+    previewLineHeight: 1.45,
+    autoCloseBrackets: true
   }
 });
