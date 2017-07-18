4ed21d5f6d8c4e91475534483a812e52209d4ddc
JonSn0w <therealbrettstevenson@gmail.com>
2017-07-18 05:14:00 -0700

    :nut_and_bolt: Add additional config options

--- config.js
+++ config.js
@@ -3,23 +3,24 @@ const Config = require('electron-config');
 
 module.exports = new Config ({
   defaults: {
-    firstLaunch: false,
     showMenu: false,
     showToolbar: false,
     livePreview: true,
-    syncScroll: false,
+    syncScroll: true,
+    isFullscreen: false,
     keepInTray: false,
     editorTheme: 'one-dark',
-    editorFont: 'default',
-    editorFontSize: 10,
+    editorFont: 'monospaced',
+    editorFontSize: 9.5,
     editorLineHeight: 1.5,
     tabSize: 2,
     lineNumbers: false,
     lineWrapping: true,
     previewMode: 'markdown',
-    previewFont: 'default',
+    previewFont: 'sans-serif',
     previewFontSize: 10,
     previewLineHeight: 1.45,
+    previewFrontMatter: false,
     autoCloseBrackets: true
   }
 });
