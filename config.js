3b896a152b60b1983a2dbbcf4c9fbffe7522762e
JonSn0w <therealbrettstevenson@gmail.com>
2017-09-19 07:50:42 -0700

    :nut_and_bolt: Update default configuration

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ module.exports = new Config ({
     isMaximized: false,
     // Editor
     editorTheme: 'one-dark',
-    editorFontSize: 9.5,
+    editorFontSize: 11.5,
     dynamicEditor: false,
     tabSize: 2,
     lineNumbers: false,
@@ -22,7 +22,7 @@ module.exports = new Config ({
     // Preview
     previewMode: 'markdown',
     previewProfile: 'Default',
-    previewFontSize: 10,
+    previewFontSize: 13.5,
     previewLineHeight: 1.45,
     customCSS: false,
     mathRendering: true,
