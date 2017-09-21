ef45c6b21e5f733b88697bd1828215f87f3e240a
JonSn0w <therealbrettstevenson@gmail.com>
2017-09-21 08:00:23 -0700

    :truck: Move native app menu creation to separate file

--- main.js
+++ main.js
@@ -27,23 +27,25 @@ const dialog = electron.dialog;
 const shell = electron.shell;
 const BrowserWindow = electron.BrowserWindow;
 const {Menu, MenuItem} = require('electron');
-const tray = require('./tray');
-const func = require('./js/functions');
 const mod = require('./package.json');
 const settings = require('./config');
+const tray = require('./tray');
+const func = require('./js/functions');
+const menuTemplate = require('./js/menu');
 const keepInTray = settings.get('keepInTray');
 const fs = require('fs');
 const path = require('path');
 const window = require('electron-window');
 const localShortcut = require('electron-localshortcut');
 const windowStateManager = require('electron-window-state');
+const packageJSON = require(__dirname + '/package.json');
 const mainPage = (`file://${__dirname}/index.html`);
 const yargs = require('yargs')
 const args = yargs(process.argv)
     .alias('d', 'dev')
     .argv
 
-// Keep a global reference of the window object
+// Keep a global reference of the window objects
 var windows = new Set();
 let isQuitting = false;
 let windowState, mainWindow;
@@ -158,92 +160,7 @@ function menuThemes() {
   return themes;
 }
 
-//Set native menubar
-var template = [
-  {label: "&File", submenu: [
-    {label: "New", accelerator: "CmdOrCtrl+N", click: () => { createWindow(); }},
-    {label: "Open", accelerator: "CmdOrCtrl+O", click: () => { sendShortcut('file-open'); }},
-    {type: "separator"},
-    {label: "Save", accelerator: "CmdOrCtrl+S", click: () => { sendShortcut('file-save'); }},
-    {label: "Save As", accelerator: "CmdOrCtrl+Shift+S", click: () => { sendShortcut('file-save-as'); }},
-    {label: "Export to PDF", click: () => { sendShortcut('file-pdf'); }},
-    {type: "separator"},
-    {label: "Show in File Manager", click: () => { ipc.send('open-file-manager'); }},
-    {type: "separator"},
-    {label: "Settings", accelerator: "CmdOrCtrl+,", click: () => { sendShortcut('ctrl+,'); }},
-    {type: "separator"},
-    {label: "Quit", accelerator: "CmdOrCtrl+Q", click: () => { sendShortcut('ctrl+q'); }}
-  ]},
-  {label: "&Edit", submenu: [
-    {label: "Undo", accelerator: "CmdOrCtrl+Z", role: "undo"},
-    {label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", role: "redo"},
-    {type: "separator"},
-    {label: "Cut", accelerator: "CmdOrCtrl+X", role: "cut"},
-    {label: "Copy", accelerator: "CmdOrCtrl+C", role: "copy"},
-    {label: "Paste", accelerator: "CmdOrCtrl+V", role: "paste"},
-    {label: "Select All", accelerator: "CmdOrCtrl+A", click: () => { sendShortcut('ctrl+a'); }},
-    {type: "separator"},
-    {label: "Find", accelerator: "CmdOrCtrl+F", click: () => { sendShortcut('ctrl+f'); }},
-    {label: "Replace", accelerator: "CmdOrCtrl+Shift+F", click: () => { sendShortcut('ctrl+shift+f'); }},
-    {type: "separator"},
-    {label: "Auto-Indent", accelerator: "CmdOrCtrl+Shift+A", click: () => { sendShortcut('ctrl+shift+a'); }},
-    {label: "Indent Less", accelerator: "CmdOrCtrl+Left", click: () => { sendShortcut('ctrl+left'); }},
-    {label: "Indent More", accelerator: "CmdOrCtrl+Right", click: () => { sendShortcut('ctrl+right'); }},
-    {type: "separator"},
-    {label: "Toggle Comment", accelerator: "CmdOrCtrl+/", click: () => { sendShortcut('ctrl+/'); }},
-    {type: "separator"},
-    {label: "Insert YAML-frontmatter", accelerator: "CmdOrCtrl+Shift+Y", click: () => { sendShortcut('insert-yaml'); }}
-  ]},
-  {label: "&View", submenu: [
-    {label: "Reload", accelerator:"CmdOrCtrl+R", click: () => { sendShortcut('ctrl+r'); }},
-    {type: "separator"},
-    {label: "Toggle Menu", accelerator:"CmdOrCtrl+M", click: () => { sendShortcut('ctrl+m'); }},
-    {label: "Toggle Toolbar", accelerator:"CmdOrCtrl+.", click: () => { sendShortcut('ctrl+.'); }},
-    {label: "Toggle Preview", accelerator:"CmdOrCtrl+P", click: () => { sendShortcut("ctrl+p"); }},
-    {label: "Toggle Full Screen", accelerator:"F11", click: () => { sendShortcut("mazimize"); }},
-    {type: "separator"},
-    {label: "Themes" },
-    {type: "separator"},
-    {label: "Preview Mode", submenu: [
-      {label: "Markdown", click: () => { sendShortcut('markdown-preview'); }},
-      {label: "HTML", click: () => { sendShortcut('html-preview'); }}
-    ]},
-    {type: "separator"},
-    {role: 'toggledevtools'}
-  ]},
-  {label: "&Window", submenu: [
-    {label: "Minimize", click: () => {
-      BrowserWindow.getFocusedWindow().minimize();
-    }},
-    {label: "Zoom", click: () => {
-      toggleMaximize();
-    }},
-    {type: "separator"},
-    {label: "Bring to Front", click: () => {
-      windows[0].show();
-    }}
-  ]},
-  {label: "&Help", role: 'help', submenu: [
-    {label: "Markdown Help", click: () => {
-      sendShortcut('markdown-modal');
-    }},
-    {type: "separator"},
-    {label: "Documentation", click: () => {
-      shell.openExternal('https://JonSn0w.github.io/Hyde/documentation');
-    }},
-    {label: "Keybindings", click: () => {
-      shell.openExternal('https://JonSn0w.github.io/Hyde/documentation#keybindings');
-    }},
-    {label: "Report Issue", click: () => {
-      shell.openExternal('https://github.com/JonSn0w/Hyde/issues/new');
-    }},
-    {type: 'separator'},
-    {label: "About Hyde", click: () => {
-      sendShortcut('about-modal');
-    }}
-  ]}
-];
-
+var template = menuTemplate();
 if (process.platform === 'darwin') {
   const name = app.getName();
   template.unshift({
@@ -283,7 +200,7 @@ function sendShortcut(cmd) {
   focusedWindow.webContents.send(cmd);
 }
 
-// Register local keyboard shortcuts for formatting Markdown
+// Register local keyboard shortcuts
 localShortcut.register('CmdOrCtrl+Shift+a', () => { sendShortcut('ctrl+shift+a'); });
 localShortcut.register('CmdOrCtrl+b', () => { sendShortcut('ctrl+b'); });
 localShortcut.register('CmdOrCtrl+d', () => { sendShortcut('ctrl+d'); });
@@ -311,9 +228,8 @@ localShortcut.register('CmdOrCtrl+left', () => { sendShortcut('ctrl+left'); });
 localShortcut.register('CmdOrCtrl+right', () => { sendShortcut('ctrl+right'); });
 localShortcut.register('CmdOrCtrl+down', () => { sendShortcut('ctrl+down'); });
 
-// Called when Electron has finished initialization
+// Called after initialization
 app.on('ready', function() {
-  require('devtron').install();
   // Create native application menu
   const menu = Menu.buildFromTemplate(template);
   Menu.setApplicationMenu(menu);
@@ -329,8 +245,12 @@ app.on('ready', function() {
   // mainWindow.showUrl(path.join(__dirname, 'index.html'));
   windowState.manage(mainWindow);
   windows.add(mainWindow);
+  tray.create(mainWindow);
   // Show Dev Tools
-  if(args.dev) { mainWindow.webContents.openDevTools(); }
+  if(args.dev) {
+    require('devtron').install();
+    mainWindow.webContents.openDevTools();
+  }
   mainWindow.once('ready-to-show', () => {
     mainWindow.show();
   });
@@ -378,7 +298,7 @@ app.on('ready', function() {
     console.error(err);
   });
 
-  //Listen for crashes
+  // Listen for crashes
   mainWindow.webContents.on('crashed', (err) => {
     console.error(err);
   });
@@ -386,23 +306,8 @@ app.on('ready', function() {
 
 // Listen for uncaughtExceptions
 process.on('uncaughtException', (err) => {
-  console.error(er)
+  console.error(err)
 });
-
-let rightClickPos = null;
-const contextMenu = new Menu();
-contextMenu.append(new MenuItem({ role: 'undo' }))
-contextMenu.append(new MenuItem({ role: 'redo' }))
-contextMenu.append(new MenuItem({ type: 'separator' }))
-contextMenu.append(new MenuItem({ label: "Cut", role: "cut" }))
-contextMenu.append(new MenuItem({ label: "Copy", role: "copy" }))
-contextMenu.append(new MenuItem({ label: "Paste", role: "paste" }))
-contextMenu.append(new MenuItem({ label: "Select All", click: () => { sendShortcut('ctrl+a'); } }))
-contextMenu.append(new MenuItem({ type: 'separator' }))
-contextMenu.append(new MenuItem({ label: 'Show in File Manager', click: () => { sendShortcut('open-file-manager'); } }))
-contextMenu.append(new MenuItem({ type: 'separator' }))
-contextMenu.append(new MenuItem({ label: 'Inspect Element', click: () => { mainWindow.inspectElement(rightClickPos.x, rightClickPos.y); }}))
-
 
 app.on('browser-window-created', function(event, win) {
   win.webContents.on('context-menu', function(e, params) {
@@ -420,27 +325,20 @@ const appVersion = exports.appVersion = () => {
   return app.getVersion();
 }
 
-const Tray = electron.Tray
-let appIcon = null
-
-ipc.on('put-in-tray', function (event) {
-  const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png'
-  const iconPath = path.join(__dirname, iconName)
-  appIcon = new Tray(iconPath)
-  const contextMenu = Menu.buildFromTemplate([{
-    label: 'Remove',
-    click: function () {
-      event.sender.send('tray-removed')
-    }
-  }])
-  appIcon.setToolTip('Electron Demo in the tray.')
-  appIcon.setContextMenu(contextMenu)
-})
-
-ipc.on('remove-tray', function () {
-  appIcon.destroy()
-})
-
 app.on('window-all-closed', function () {
   if (appIcon) appIcon.destroy()
 })
+
+let rightClickPos = null;
+const contextMenu = new Menu();
+contextMenu.append(new MenuItem({ role: 'undo' }))
+contextMenu.append(new MenuItem({ role: 'redo' }))
+contextMenu.append(new MenuItem({ type: 'separator' }))
+contextMenu.append(new MenuItem({ label: "Cut", role: "cut" }))
+contextMenu.append(new MenuItem({ label: "Copy", role: "copy" }))
+contextMenu.append(new MenuItem({ label: "Paste", role: "paste" }))
+contextMenu.append(new MenuItem({ label: "Select All", click: () => { sendShortcut('ctrl+a'); } }))
+contextMenu.append(new MenuItem({ type: 'separator' }))
+contextMenu.append(new MenuItem({ label: 'Show in File Manager', click: () => { sendShortcut('open-file-manager'); } }))
+contextMenu.append(new MenuItem({ type: 'separator' }))
+contextMenu.append(new MenuItem({ label: 'Inspect Element', click: () => { mainWindow.inspectElement(rightClickPos.x, rightClickPos.y); }}))
