e53e3ec539146ff24325703f95a0c43f25f92a26
JonSn0w <therealbrettstevenson@gmail.com>
2017-09-23 03:07:36 -0700

    :inbox_tray: Improve app tray functionality

--- tray.js
+++ tray.js
@@ -6,33 +6,33 @@ var iconPath = path.join(__dirname, 'img/icon/ico/icon.ico');
 let tray = null;
 
 exports.create = function(mainWindow) {
-  if (process.platform === 'darwin' || tray) {
-    return;
-  }
+	if(process.platform === 'darwin' || tray) {
+		return;
+	}
 
-  const toggleHide = () => {
-    if (mainWindow.isVisible()) {
-      mainWindow.hide();
-    } else {
-      mainWindow.show();
-    }
-  };
+	const toggleHide = () => {
+		if(mainWindow.isVisible()) {
+			mainWindow.hide();
+		} else {
+			mainWindow.show();
+		}
+	};
 
-  const contextMenu = electron.Menu.buildFromTemplate([
-    {label: 'Show/Hide', click() { toggleHide(); }},
-    { type: 'separator' },
-    { label: 'Quit', click() { app.quit(); }}
-  ]);
+	const contextMenu = electron.Menu.buildFromTemplate([
+		{ label: 'Show/Hide', click() { toggleHide(); }},
+		{ type: 'separator' },
+		{ label: 'Quit', click() { app.quit(); }}
+	]);
 
-  if (process.platform === 'darwin')
-    iconPath = path.join(__dirname, 'img/icon/icns/icon.icns');
-  else if(process.platform === 'win32')
-    iconPath = path.join(__dirname, 'img/icon/ico/icon.ico');
-  else
-    iconPath = path.join(__dirname, 'img/icon/png/48x48.png');
+	if(process.platform === 'darwin')
+		iconPath = path.join(__dirname, 'img/icon/icns/icon.icns');
+	else if(process.platform === 'win32')
+		iconPath = path.join(__dirname, 'img/icon/ico/icon.ico');
+	else
+		iconPath = path.join(__dirname, 'img/icon/png/48x48.png');
 
-  tray = new electron.Tray(iconPath);
-  tray.setToolTip('Hyde');
-  tray.setContextMenu(contextMenu);
-  tray.on('click', toggleHide);
+	tray = new electron.Tray(iconPath);
+	tray.setToolTip('Hyde');
+	tray.setContextMenu(contextMenu);
+	tray.on('click', toggleHide);
 };
