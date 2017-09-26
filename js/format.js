f656f158c3d38716a144ff3a819abccb80a610e8
JonSn0w <therealbrettstevenson@gmail.com>
2017-09-26 12:11:20 -0700

    :rotating_light: Remove linter warnings

--- js/format.js
+++ js/format.js
@@ -1,29 +1,29 @@
 
 var insertTexts = {
-  link: ["[", "](#url#)"],
-  image: ["![", "](#url#)"],
-  horizontalRule: ["", "\n-------------------\n"]
+  link: ['[', '](#url#)'],
+  image: ['![', '](#url#)'],
+  horizontalRule: ['', '\n-------------------\n']
 };
 
 
 function toggleFormat(type) {
   'use strict';
   let modifiers;
-  if(type === "strong")
-    modifiers = ["**", "__"];
-  else if(type === "em")
-    modifiers = ["*", "_"];
-  else if(type === "strikethrough")
-    modifiers = ["~~"];
-  else if(type === "code") //TODO inline vs code block
-    modifiers = ["`"];
-  cm.operation(() => { toggleState(type, modifiers); })
+  if(type === 'strong')
+    modifiers = ['**', '__'];
+  else if(type === 'em')
+    modifiers = ['*', '_'];
+  else if(type === 'strikethrough')
+    modifiers = ['~~'];
+  else if(type === 'code') //TODO: Handle inline vs code block
+    modifiers = ['`'];
+  cm.operation(() => { toggleState(type, modifiers); });
 }
 
 function toggleState(type, modifiers) {
   if(modifiers.length === 0) return;
-  let startPoint = cm.getCursor("start");
-  let endPoint = cm.getCursor("end");
+  let startPoint = cm.getCursor('start');
+  let endPoint = cm.getCursor('end');
   // get word at cursor if there's no selection
   if(!cm.somethingSelected()) {
     var word = cm.findWordAt(cm.getCursor());
@@ -39,7 +39,7 @@ function toggleState(type, modifiers) {
       text = selection.substring(modiLength, selection.length - modiLength),
       cursorPOS;
   if(currentState && currentState.includes(type)) {
-    modifiers.forEach(function(modi, index) {
+    modifiers.forEach(function(modi) {
       if(selection === modi+text+modi) {
         text = selection.substring(modiLength, selection.length - modiLength);
         cursorPOS = {line:endPoint.line, char:endPoint.ch - (modiLength*2)};
@@ -48,25 +48,11 @@ function toggleState(type, modifiers) {
         endPoint = outerEnd;
         text = outerSelection.substring(modiLength, outerSelection.length - modiLength);
         cursorPOS = {line:endPoint.line, char:endPoint.ch - (modiLength*2)};
-      } //else if(outerSelection.includes(modi+selection)) {
-      //   startPoint = outerStart;
-      //   endPoint = findBreak(endPoint, 1);
-      //   text = getRange(startPoint, endPoint).substring(modiLength, outerSelection.length);
-      //   txt = text;
-      //   select = cm.getRange(startPoint, endPoint);
-      //   cursorPOS = {line:endPoint.line, char:endPoint.ch - (modiLength*2)};
-      // } else if(outerSelection.includes(selection+modi)) {
-      //   endPoint = outerEnd;
-      //   startPoint = findBreak(startPoint, 1);
-      //   text = getRange(startPoint, endPoint).substring(0, outerSelection.length-modiLength);
-      //   txt = text;
-      //   select = cm.getRange(startPoint, endPoint);
-      //   cursorPOS = {line:endPoint.line, char:endPoint.ch - (modiLength*2)};
-      // }
+      }
     });
     if(text === undefined) {
-      startPoint = findBreak(startPoint, "1");
-      endPoint = findBreak(endPoint, "-1");
+      startPoint = findBreak(startPoint, '1');
+      endPoint = findBreak(endPoint, '-1');
       text = modifiers[0]+cm.getRange(startPoint, endPoint)+modifiers[0];
       cursorPOS = {line:endPoint.line, char:endPoint.ch + (modiLength*2)};
     }
@@ -84,7 +70,7 @@ function findBreak(point, dir) {
   var char, temp;
   while(char !== ' ') {
     temp = {line:point.line, ch:point.ch};
-    if(dir === "-1") {
+    if(dir === '-1') {
       point = {line:point.line, ch:(point.ch + 1)};
       char = cm.getRange(temp, point);
     } else {
@@ -96,86 +82,86 @@ function findBreak(point, dir) {
 }
 
 function getState(cm, pos) {
-  pos = pos || cm.getCursor("start");
+  pos = pos || cm.getCursor('start');
   var stat = cm.getTokenAt(pos);
   if(!stat.type) return {};
-  var types = stat.type.split(" ");
+  var types = stat.type.split(' ');
   var ret = {}, data, text;
   for(var i = 0; i < types.length; i++) {
     data = types[i];
-    if(data === "strong") {
+    if(data === 'strong') {
       ret.bold = true;
-    } else if(data === "variable-2") {
+    } else if(data === 'variable-2') {
       text = cm.getLine(pos.line);
       if(/^\s*\d+\.\s/.test(text)) {
-        ret["ordered-list"] = true;
+        ret['ordered-list'] = true;
       } else {
-        ret["unordered-list"] = true;
+        ret['unordered-list'] = true;
       }
-    } else if(data === "atom") {
+    } else if(data === 'atom') {
       ret.quote = true;
-    } else if(data === "em") {
+    } else if(data === 'em') {
       ret.italic = true;
-    } else if(data === "quote") {
+    } else if(data === 'quote') {
       ret.quote = true;
-    } else if(data === "strikethrough") {
+    } else if(data === 'strikethrough') {
       ret.strikethrough = true;
-    } else if(data === "comment") {
+    } else if(data === 'comment') {
       text = cm.getLine(pos.line);
       if(text.indexOf('`') > -1) {
           ret.code = true;
       } else {
           ret.comment = true;
       }
-    } else if(data === "link") {
+    } else if(data === 'link') {
       ret.link = true;
-    } else if(data === "tag") {
+    } else if(data === 'tag') {
       ret.image = true;
-    } else if(data.match(/^header(\-[1-6])?$/)) {
-      ret[data.replace("header", "heading")] = true;
+    } else if(data.match(/^header(-[1-6])?$/)) {
+      ret[data.replace('header', 'heading')] = true;
     }
   }
   return ret;
 }
 
 function toggleBlockquote() {
-  _toggleLine(cm, "quote");
+  _toggleLine(cm, 'quote');
 }
 
 function toggleComment(editor) {
-  _toggleLine(cm, "comment");
+  _toggleLine(cm, 'comment');
 }
 
 function toggleUnorderedList(editor) {
-  _toggleLine(cm, "unordered-list");
+  _toggleLine(cm, 'unordered-list');
 }
 
 function toggleOrderedList(editor) {
-  _toggleLine(cm, "ordered-list");
+  _toggleLine(cm, 'ordered-list');
 }
 
 
 function _toggleLine(cm, name) {
   if(/editor-preview-active/.test(cm.getWrapperElement().lastChild.className)) return;
-  var startPoint = cm.getCursor("start"),
-      endPoint = cm.getCursor("end"),
+  var startPoint = cm.getCursor('start'),
+      endPoint = cm.getCursor('end'),
       stat = getState(cm);
   var repl = {
-    "quote": /^(\s*)\>\s+/,
-    "unordered-list": /^(\s*)(\*|\-|\+)\s+/,
-    "ordered-list": /^(\s*)\d+\.\s+/,
-    "comment": /<!-- (.*) -->/
+    'quote': /^(\s*)>\s+/,
+    'unordered-list': /^(\s*)(\*|-|\+)\s+/,
+    'ordered-list': /^(\s*)\d+\.\s+/,
+    'comment': /<!-- (.*) -->/
   };
   var map = {
-    "quote": "> ",
-    "unordered-list": "* ",
-    "ordered-list": "1. ",
-    "comment": ["<!-- ", " -->"]
+    'quote': '> ',
+    'unordered-list': '* ',
+    'ordered-list': '1. ',
+    'comment': ['<!-- ', ' -->']
   };
   for(var i = startPoint.line; i <= endPoint.line; i++) {
     var text = cm.getLine(i);
     if(stat[name])
-      text = text.replace(repl[name], "$1");
+      text = text.replace(repl[name], '$1');
     else if(map[name].constructor === Array)
       text = map[name][0] + text + map[name][1];
     else
@@ -186,7 +172,6 @@ function _toggleLine(cm, name) {
 }
 
 function insert(obj) {
-    var stat = getState(cm);
     if(obj === 'link')
       insertLink(cm, obj);
     else if(obj === 'image')
@@ -198,59 +183,58 @@ function insert(obj) {
 
 function createTable(cols, rows, align) {
   if(!cols || !rows) return notify('Invalid table parameters', 'error');
-  var startPoint = cm.getCursor("start"),
+  var startPoint = cm.getCursor('start'),
       text = cm.getLine(startPoint.line),
       start = text.slice(0, startPoint.ch),
       end = text.slice(startPoint.ch),
-      table = separator = body = "| ",
-      alignment = " | ";
+      table = separator = body = '| ',
+      alignment = ' | ';
   if(align === 'center') {
-    separator = "|:"
-    alignment = ":|:"
-  }
-  else if(align === 'right') {
-    alignment = ":| "
+    separator = '|:';
+    alignment = ':|:';
+  } else if(align === 'right') {
+    alignment = ':| ';
   }
 
   for(let i = 1; i <= cols; i++) {
-    table += "Column "+i+" | ";
-    separator += "--------"+alignment;
-    body += " Text     | ";
+    table += 'Column '+i+' | ';
+    separator += '--------'+alignment;
+    body += ' Text     | ';
   }
-  table += "  \n"+separator.slice(0,-1)+"  \n";
+  table += '  \n'+separator.slice(0,-1)+'  \n';
   for(let i = 0; i < rows; i++) {
-    table += body+"  \n";
+    table += body+'  \n';
   }
   cm.replaceSelection(start + table + end);
 }
 
 // function for adding heading
 function toggleHeading() {
-  _toggleHeading("larger");
+  _toggleHeading('larger');
 }
 
 function _toggleHeading(direction) {
-  var startPoint = cm.getCursor("start"),
-      endPoint = cm.getCursor("end");
+  var startPoint = cm.getCursor('start'),
+      endPoint = cm.getCursor('end');
   for(var i = startPoint.line; i <= endPoint.line; i++) {
     var text = cm.getLine(i);
     var currHeadingLevel = text.search(/[^#]/);
     if(direction !== undefined) {
       if(currHeadingLevel <= 0) {
-        if(direction === "smaller") {
-          text = "###### " + text;
+        if(direction === 'smaller') {
+          text = '###### ' + text;
         } else {
-          text = "# " + text;
+          text = '# ' + text;
         }
-      } else if(currHeadingLevel === 6 && direction === "larger") {
+      } else if(currHeadingLevel === 6 && direction === 'larger') {
           text = text.substr(7);
-      } else if(currHeadingLevel === 1 && direction === "smaller") {
+      } else if(currHeadingLevel === 1 && direction === 'smaller') {
           text = text.substr(2);
       } else {
-        if(direction === "smaller") {
+        if(direction === 'smaller') {
           text = text.substr(1);
         } else {
-          text = "#" + text;
+          text = '#' + text;
         }
       }
     }
@@ -260,13 +244,13 @@ function _toggleHeading(direction) {
 }
 
 function selectWord() {
-  var startPoint, endPoint;
+  var word, startPoint, endPoint;
   if(!cm.somethingSelected()) {
-    var word = cm.findWordAt(cm.getCursor());
+    word = cm.findWordAt(cm.getCursor());
     startPoint = word.anchor;
     endPoint = word.head;
   } else {
-    var word = cm.findWordAt(cm.getSelection());
+    word = cm.findWordAt(cm.getSelection());
     startPoint = word.anchor;
     endPoint = word.head;
   }
@@ -275,8 +259,8 @@ function selectWord() {
 
 // Insert link or image-link syntax
 function insertLink(cm, type) {
-  var startPoint = cm.getCursor("start"),
-      endPoint = cm.getCursor("end"),
+  var startPoint = cm.getCursor('start'),
+      endPoint = cm.getCursor('end'),
       modi = ['[','](http://)'];
   if(type === 'image')
     modi[0] = '!'+modi[0];
@@ -308,18 +292,17 @@ function insertHorizontalLine() {
 // Inserts YAML-frontmatter from template file
 function insertFrontMatter() {
   var path = settings.get('frontMatterTemplate'),
-      extensions = ["yaml", "yml", "md", "markdown", "txt", "text"];
+      extensions = ['yaml', 'yml', 'md', 'markdown', 'txt', 'text'];
   if(path.length < 1) {
     setFrontMatterTemplate();
-    return notify("There is no specified frontmatter template", "error");
+    return notify('There is no specified frontmatter template', 'error');
   }
   if(!extensions.includes(path.split('.').pop()))
-    return notify("Invalid specified template file", "error");
+    return notify('Invalid specified template file', 'error');
   fs.readFile(path, 'utf8', function (err, data) {
-    if(err) return notify(err, "error");
+    if(err) return notify(err, 'error');
     else {
       cm.execCommand('goDocStart');
-      var text = insertDate(data, formatDate());
       cm.replaceSelection(insertDate(data, formatDate()));
       cm.execCommand('newlineAndIndent');
       cm.setCursor(1);
@@ -330,8 +313,8 @@ function insertFrontMatter() {
 }
 
 function insertEmoji(emoji) {
-  var startPoint = cm.getCursor("start"),
-      endPoint = cm.getCursor("end");
+  var startPoint = cm.getCursor('start'),
+      endPoint = cm.getCursor('end');
   if(!cm.somethingSelected()) {
     var word = cm.findWordAt(cm.getCursor());
     startPoint = word.anchor;
@@ -361,18 +344,18 @@ function formatDate() {
 // Automatically sets 'date' parameters to the current date
 function insertDate(doc, date) {
   var arr = [],
-      str = "";
+      str = '';
   doc.split('\n').forEach((line) => {
     arr.push(line);
     line = line.replace(/ \n/g,'');
     if(line.match(/date:/i))
-      line = line+" "+date;
+      line = line+' '+date;
     str += line+'\n';
   });
   return str;
 }
 
 function removeYAMLPreview(preview) {
-    var re = new RegExp(/((---\n))(\w|\d|\n|[().,\-:;@#$%^&*\[\]\"\'+–\/\/®°⁰!?{}|`~]| )+?((---))/gm, "mg");
-    return preview.replace(re, "<br>");
+  var re = new RegExp(/((---\n))(\w|\d|\n|[().,\-:;@#$%^&*[\]''+–//®°⁰!?{}|`~]| )+?((---))/gm, 'mg');
+  return preview.replace(re, '');
 }
--- js/menu.js
+++ js/menu.js
@@ -1,88 +1,86 @@
-const electron = require('electron');
-const remote = electron.remote;
 
 function createMenuTemplate() {
 	var template = [
-		{label: "&File", submenu: [
-			{label: "New", accelerator: "CmdOrCtrl+N", click: () => { createWindow(); }},
-			{label: "Open", accelerator: "CmdOrCtrl+O", click: () => { main.sendShortcut('file-open'); }},
-			{type: "separator"},
-			{label: "Save", accelerator: "CmdOrCtrl+S", click: () => { main.sendShortcut('file-save'); }},
-			{label: "Save As", accelerator: "CmdOrCtrl+Shift+S", click: () => { main.sendShortcut('file-save-as'); }},
-			{label: "Export to PDF", click: () => { main.sendShortcut('file-pdf'); }},
-			{type: "separator"},
-			{label: "Show in File Manager", click: () => { ipc.send('open-file-manager'); }},
-			{type: "separator"},
-			{label: "Settings", accelerator: "CmdOrCtrl+,", click: () => { main.sendShortcut('ctrl+,'); }},
-			{type: "separator"},
-			{label: "Quit", accelerator: "CmdOrCtrl+Q", click: () => { main.sendShortcut('ctrl+q'); }}
+		{label: '&File', submenu: [
+			{label: 'New', accelerator: 'CmdOrCtrl+N', click: () => { createWindow(); }},
+			{label: 'Open', accelerator: 'CmdOrCtrl+O', click: () => { main.sendShortcut('file-open'); }},
+			{type: 'separator'},
+			{label: 'Save', accelerator: 'CmdOrCtrl+S', click: () => { main.sendShortcut('file-save'); }},
+			{label: 'Save As', accelerator: 'CmdOrCtrl+Shift+S', click: () => { main.sendShortcut('file-save-as'); }},
+			{label: 'Export to PDF', click: () => { main.sendShortcut('file-pdf'); }},
+			{type: 'separator'},
+			{label: 'Show in File Manager', click: () => { ipc.send('open-file-manager'); }},
+			{type: 'separator'},
+			{label: 'Settings', accelerator: 'CmdOrCtrl+,', click: () => { main.sendShortcut('toggle-settings'); }},
+			{type: 'separator'},
+			{label: 'Quit', accelerator: 'CmdOrCtrl+Q', click: () => { main.sendShortcut('win-close'); }}
 		]},
-		{label: "&Edit", submenu: [
-			{label: "Undo", accelerator: "CmdOrCtrl+Z", role: "undo"},
-			{label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", role: "redo"},
-			{type: "separator"},
-			{label: "Cut", accelerator: "CmdOrCtrl+X", role: "cut"},
-			{label: "Copy", accelerator: "CmdOrCtrl+C", role: "copy"},
-			{label: "Paste", accelerator: "CmdOrCtrl+V", role: "paste"},
-			{label: "Select All", accelerator: "CmdOrCtrl+A", click: () => { main.sendShortcut('ctrl+a'); }},
-			{type: "separator"},
-			{label: "Find", accelerator: "CmdOrCtrl+F", click: () => { main.sendShortcut('ctrl+f'); }},
-			{label: "Replace", accelerator: "CmdOrCtrl+Shift+F", click: () => { main.sendShortcut('ctrl+shift+f'); }},
-			{type: "separator"},
-			{label: "Auto-Indent", accelerator: "CmdOrCtrl+Shift+A", click: () => { main.sendShortcut('ctrl+shift+a'); }},
-			{label: "Indent Less", accelerator: "CmdOrCtrl+Left", click: () => { main.sendShortcut('ctrl+left'); }},
-			{label: "Indent More", accelerator: "CmdOrCtrl+Right", click: () => { main.sendShortcut('ctrl+right'); }},
-			{type: "separator"},
-			{label: "Toggle Comment", accelerator: "CmdOrCtrl+/", click: () => { main.sendShortcut('ctrl+/'); }},
-			{type: "separator"},
-			{label: "Insert YAML-frontmatter", accelerator: "CmdOrCtrl+Shift+Y", click: () => { main.sendShortcut('insert-yaml'); }}
+		{label: '&Edit', submenu: [
+			{label: 'Undo', accelerator: 'CmdOrCtrl+Z', role: 'undo'},
+			{label: 'Redo', accelerator: 'CmdOrCtrl+Shift+Z', role: 'redo'},
+			{type: 'separator'},
+			{label: 'Cut', accelerator: 'CmdOrCtrl+X', role: 'cut'},
+			{label: 'Copy', accelerator: 'CmdOrCtrl+C', role: 'copy'},
+			{label: 'Paste', accelerator: 'CmdOrCtrl+V', role: 'paste'},
+			{label: 'Select All', accelerator: 'CmdOrCtrl+A', click: () => { main.sendShortcut('select-all'); }},
+			{type: 'separator'},
+			{label: 'Find', accelerator: 'CmdOrCtrl+F', click: () => { main.sendShortcut('search-find'); }},
+			{label: 'Replace', accelerator: 'CmdOrCtrl+Shift+F', click: () => { main.sendShortcut('search-replace'); }},
+			{type: 'separator'},
+			{label: 'Auto-Indent', accelerator: 'CmdOrCtrl+Shift+A', click: () => { main.sendShortcut('auto-indent'); }},
+			{label: 'Indent Less', accelerator: 'CmdOrCtrl+Left', click: () => { main.sendShortcut('indent-less'); }},
+			{label: 'Indent More', accelerator: 'CmdOrCtrl+Right', click: () => { main.sendShortcut('indent-more'); }},
+			{type: 'separator'},
+			{label: 'Toggle Comment', accelerator: 'CmdOrCtrl+/', click: () => { main.sendShortcut('insert-comment'); }},
+			{type: 'separator'},
+			{label: 'Insert YAML-frontmatter', accelerator: 'CmdOrCtrl+Shift+Y', click: () => { main.sendShortcut('insert-yaml'); }}
 		]},
-		{label: "&View", submenu: [
-			{label: "Reload", accelerator:"CmdOrCtrl+R", click: () => { main.sendShortcut('ctrl+r'); }},
-			{type: "separator"},
-			{label: "Toggle Menu", accelerator:"CmdOrCtrl+M", click: () => { main.sendShortcut('ctrl+m'); }},
-			{label: "Toggle Toolbar", accelerator:"CmdOrCtrl+.", click: () => { main.sendShortcut('ctrl+.'); }},
-			{label: "Toggle Preview", accelerator:"CmdOrCtrl+P", click: () => { main.sendShortcut("ctrl+p"); }},
-			{label: "Toggle Full Screen", accelerator:"F11", click: () => { main.sendShortcut("mazimize"); }},
-			{type: "separator"},
-			{label: "Themes" },
-			{type: "separator"},
-			{label: "Preview Mode", submenu: [
-				{label: "Markdown", click: () => { main.sendShortcut('markdown-preview'); }},
-				{label: "HTML", click: () => { main.sendShortcut('html-preview'); }}
+		{label: '&View', submenu: [
+			{label: 'Reload', accelerator:'CmdOrCtrl+R', click: () => { main.sendShortcut('win-reload'); }},
+			{type: 'separator'},
+			{label: 'Toggle Menu', accelerator:'CmdOrCtrl+M', click: () => { main.sendShortcut('toggle-menu'); }},
+			{label: 'Toggle Toolbar', accelerator:'CmdOrCtrl+.', click: () => { main.sendShortcut('toggle-toolbar'); }},
+			{label: 'Toggle Preview', accelerator:'CmdOrCtrl+P', click: () => { main.sendShortcut('toggle-preview'); }},
+			{label: 'Toggle Full Screen', accelerator:'F11', click: () => { main.sendShortcut('mazimize'); }},
+			{type: 'separator'},
+			{label: 'Themes' },
+			{type: 'separator'},
+			{label: 'Preview Mode', submenu: [
+				{label: 'Markdown', click: () => { main.sendShortcut('markdown-preview'); }},
+				{label: 'HTML', click: () => { main.sendShortcut('html-preview'); }}
 			]},
-			{type: "separator"},
+			{type: 'separator'},
 			{role: 'toggledevtools'}
 		]},
-		{label: "&Window", submenu: [
-			{label: "Minimize", click: () => {
+		{label: '&Window', submenu: [
+			{label: 'Minimize', click: () => {
 				BrowserWindow.getFocusedWindow().minimize();
 			}},
-			{label: "Zoom", click: () => {
+			{label: 'Zoom', click: () => {
 				toggleMaximize();
 			}},
-			{type: "separator"},
-			{label: "Bring to Front", click: () => {
+			{type: 'separator'},
+			{label: 'Bring to Front', click: () => {
 				windows[0].show();
 			}}
 		]},
-		{label: "&Help", role: 'help', submenu: [
-			{label: "Markdown Help", click: () => {
+		{label: '&Help', role: 'help', submenu: [
+			{label: 'Markdown Help', click: () => {
 				main.sendShortcut('markdown-modal');
 			}},
-			{type: "separator"},
-			{label: "Documentation", click: () => {
+			{type: 'separator'},
+			{label: 'Documentation', click: () => {
 				// shell.openExternal(packageJSON.)
 				shell.openExternal(packageJSON.docs);
 			}},
-			{label: "Keybindings", click: () => {
+			{label: 'Keybindings', click: () => {
 				shell.openExternal(packageJSON.keybindings);
 			}},
-			{label: "Report Issue", click: () => {
+			{label: 'Report Issue', click: () => {
 				shell.openExternal(packageJSON.repository.bugs);
 			}},
 			{type: 'separator'},
-			{label: "About Hyde", click: () => {
+			{label: 'About Hyde', click: () => {
 				main.sendShortcut('about-modal');
 			}}
 		]}
