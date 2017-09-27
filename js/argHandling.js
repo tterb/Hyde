ca4c89d80657d2dcb33a672ebce2d4462dbca012
JonSn0w <therealbrettstevenson@gmail.com>
2017-09-26 21:39:43 -0700

    :racehorse: Improve command-line argument handling

--- js/argHandling.js
+++ js/argHandling.js
@@ -1,25 +1,24 @@
 
 function argHandling(filePath) {
-	// Read file if given from commandline
+	// Handle files specified via the command-line
 	if(!filePath) return;
 	filePath = path.normalize(filePath);
 	var filename = path.basename(filePath);
-	var extensions = ['.md','.markdown','.mdown','.mkdn','.mkd','.mdwn','.mdtxt','.mdtext'];
 	if(filePath[0] === '~')
 		filePath.replace('~', require('os').homedir());
 	if(filePath === filename)
 		filePath = path.join(process.cwd(), filename);
+	// If specified file exists
 	if(fs.existsSync(filePath)) {
 		readFileIntoEditor(path.join(filePath));
 	} else {
-		storage.set('markdown-savefile', {'filename' : filename}, function(err) {
-			if(err) notify(err, 'error');
-		});
-		fs.writeFile(filePath, '', function(err) {
+		// Create specified file if it doesn't exist
+		storage.set('markdown-savefile', {'filename' : filePath}, (err) => { if(err) notify(err, 'error'); });
+		fs.writeFile(filePath, '', (err) => {
 			if(err) notify('An error ocurred while creating the file '+ err.message, 'error');
 		});
 		fs.readFile(filePath, 'utf-8', (err, data) => {
-			if (err) { alert('An error ocurred while opening the file '+ err.message); }
+			if(err) notify('An error ocurred while opening the file '+ err.message, 'error');
 			cm.getDoc().setValue(data);
 		});
 		this.setClean();
