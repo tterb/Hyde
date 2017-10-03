de848bee380277e0725c6685fbf13ba1e99d8c8c
JonSn0w <therealbrettstevenson@gmail.com>
2017-10-03 00:25:37 -0700

    :ok_hand: Improve CLI argument handling

--- js/argHandling.js
+++ js/argHandling.js
@@ -1,32 +1,30 @@
-
+// Handle files specified via the command-line
 function argHandling(filePath) {
-	// Handle files specified via the command-line
 	if(!filePath) return;
 	filePath = path.normalize(filePath);
 	var filename = path.basename(filePath);
 	if(filePath[0] === '~')
 		filePath.replace('~', require('os').homedir());
+  // Append the current path if only filename is given
 	if(filePath === filename)
 		filePath = path.join(process.cwd(), filename);
-	// If specified file exists
-	if(fs.existsSync(filePath)) {
-		readFileIntoEditor(path.join(filePath));
-	} else {
-		// Create specified file if it doesn't exist
-		storage.set('markdown-savefile', {'filename' : filePath}, (err) => { if(err) notify(err, 'error'); });
-		fs.writeFile(filePath, '', (err) => {
+  // Create specified file if it doesn't exist
+	if(!fs.existsSync(filePath)) {
+    fs.writeFile(filePath, '', (err) => {
 			if(err) notify('An error ocurred while creating the file '+ err.message, 'error');
 		});
-		fs.readFile(filePath, 'utf-8', (err, data) => {
-			if(err) notify('An error ocurred while opening the file '+ err.message, 'error');
-			cm.getDoc().setValue(data);
-		});
-		this.setClean();
-		options.defaultPath = filePath;
-		this.isFileLoadedInitially = true;
-		this.currentFile = filePath;
-		this.updateWindowTitle(filename);
-	}
+  }
+  storage.set('markdown-savefile', {'filename' : filePath}, (err) => { if(err) notify(err, 'error'); });
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if(err) notify('An error ocurred while opening the file '+ err.message, 'error');
+    cm.getDoc().setValue(data);
+    cm.getDoc().clearHistory();
+    this.setClean();
+    options.defaultPath = filePath;
+    this.isFileLoadedInitially = true;
+    this.currentFile = filePath;
+    this.updateWindowTitle(filePath);
+  });
 }
 
 module.exports = argHandling;
